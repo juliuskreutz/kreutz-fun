564d7abbc82c67c03a7e31a83c25ad82df5affb3
Julius Kreutz <julius@kreutz.dev>
2025-04-08 16:57:39 +0200

    Initial commit

--- flake.nix
+++ flake.nix
@@ -5,10 +5,6 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
